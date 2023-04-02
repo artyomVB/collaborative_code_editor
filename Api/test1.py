7afc49b5dab02740048fe705a5447c11539a1182
artyomVB <79898892+artyomVB@users.noreply.github.com>
2023-04-02 21:02:15 +0300

    Refactoring

--- Api/test1.py
+++
@@ -1,8 +0,0 @@
-import sqlalchemy
-
-from db import get_session
-from setup_db import UserToSession, Session
-
-with get_session() as session:
-    sessions = session.query(UserToSession, Session).filter_by(user_id=1, session_id=1, owner=True).join(Session).all()
-    print(sessions)
